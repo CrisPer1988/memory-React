36e36bc32324ebd0318e9867065f8d8ab31e42c6
CrisPer1988 <norberto.cp@hotmail.com>
2023-03-06 12:45:10 -0300

    menu-burguer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import "./styles/header.css"
 
 const Header = () => {
+
+  const [isShow, setIsShow] = useState(false)
+
+  const burguerMenu = () =>{
+    setIsShow(!isShow)
+  }
+
   return (
     <div className='container__header'>
+      <button onClick={burguerMenu} className='burguer'><i class='bx bx-menu'></i></button>
         <nav>
-            <ul className='navBar'>
+            <ul className={`navBar ${isShow ? "navBar__active" : ""}`}>
                 <li><Link to={"/"}>Básico</Link></li>
                 <li><Link to={"holaa"}>Intermedio</Link></li>
                 <li><Link to={"/pro"}>Pro</Link></li>
