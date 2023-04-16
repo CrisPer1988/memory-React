f097a1d645543e1f42085dd1f6cb323be9bd90d6
CrisPer1988 <norberto.cp@hotmail.com>
2023-04-16 12:48:47 -0300

    loading

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,23 @@ import Basico from './components/Basico'
 import Header from './components/Header'
 import Intermedio from './components/Intermedio'
 import Pro from './components/Pro'
+import { useState } from 'react'
 
 function App() {
+
+  const [load, setLoad] = useState(true)
+
+ setTimeout(() => {
+  setLoad(false)
+ }, 3000);
+    if(load) {
+      return <div className='container__load'>
+        <h2>Bienvenido a mi Memory Game</h2>
+        <h3>¿Estas listo?</h3>
+        <h6>Creado por Cristian Perez</h6>
+      </div>
+    }else 
+  
 
   return (
     <div>
