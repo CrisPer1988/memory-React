0557d2c2aff60d5f74a10136b56da7aa4f406b66
CrisPer1988 <norberto.cp@hotmail.com>
2023-04-16 12:25:00 -0300

    color burger

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
 
   return (
     <div className='container__header'>
-      <button onClick={burguerMenu} className='burguer'>{isShow ? "x" : <i class='bx bx-menu'></i>}</button>
+      <button onClick={burguerMenu} className='burguer'>{isShow ? "x" : <i className='bx bx-menu'></i>}</button>
         <nav>
             <ul className={`navBar ${isShow ? "navBar__active" : ""}`}>
                 <li className='item__navbar'><Link onClick={burguerMenu} to={"/"}>Básico</Link></li>
